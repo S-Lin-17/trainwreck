062bc90b8e3e75e6419796342a22a1e6d8f249a4
Samuel Lin <lin.sam217@gmail.com>
2022-01-23 08:09:19 -0500

    Added embedded ppt on website

--- trainwreck-frontend/src/App.js
+++ trainwreck-frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div class="center">
       <h1 class="trainwreck_headline">The Trainwrecks</h1>
-      <h1 class="take_text">A take on the RailVision McHacks Challenge 2022</h1>
+      <iframe src="https://onedrive.live.com/embed?cid=5255BD8D4EB2C599&amp;resid=5255BD8D4EB2C599%211513&amp;authkey=ACvNbjadGq8qO5Q&amp;em=2&amp;wdAr=1.7777777777777777" width="100%" height="450px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
     </div>
   );
 }
